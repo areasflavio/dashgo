19d742854b2994634377b050e51b645ea8cf6fc5
Flávio Arêas <vflavio9@gmail.com>
2021-12-19 15:57:31 -0300

    feat: use mutations to create new user data

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -9,9 +9,14 @@ import {
   VStack,
 } from '@chakra-ui/react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { useMutation } from 'react-query';
+
+import { api } from '../../services/api';
+import { queryClient } from '../../services/queryClient';
 
 import { Input } from '../../components/Form/Input';
 import Header from '../../components/Header';
@@ -37,15 +42,37 @@ const createUserFormSchema = yup.object().shape({
 });
 
 export default function UserCreate(): JSX.Element {
+  const router = useRouter();
+
+  const createUser = useMutation(
+    async (user: CreateUserFormData) => {
+      const response = await api.post('users', {
+        user: {
+          ...user,
+          created_at: new Date(),
+        },
+      });
+
+      return response.data.user;
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries('users');
+      },
+    }
+  );
+
   const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(createUserFormSchema),
   });
   const { errors } = formState;
 
-  const handleCrateUser: SubmitHandler<CreateUserFormData> = async (values) => {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (
+    values
+  ) => {
+    await createUser.mutateAsync(values);
 
-    console.log(values);
+    router.push('/users');
   };
 
   return (
@@ -61,7 +88,7 @@ export default function UserCreate(): JSX.Element {
           borderRadius={8}
           bg="gray.800"
           p={['6', '8']}
-          onSubmit={handleSubmit(handleCrateUser)}
+          onSubmit={handleSubmit(handleCreateUser)}
         >
           <Heading size="lg" fontWeight="normal">
             Criar usuário
